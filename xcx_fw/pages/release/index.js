d7220df796f5ed6a010cf1c136c6fcca3aae80cd
quincyjk <1149521502@qq.com>
2018-11-07 20:36:52 +0800

    小程序

---
+++ xcx_fw/pages/release/index.js
@@ -0,0 +1,74 @@
+// pages/release/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    arrtry:[{
+      notice: '通知',
+      pic:'http://i1.bvimg.com/650276/12c284722a4185a7.png',
+    }, {
+      notice: '公告',
+      pic: 'http://i1.bvimg.com/650276/eac505e485dc5806.png',
+    }]
+
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ xcx_fw/pages/work/index.js
@@ -0,0 +1,97 @@
+// pages/work/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    arrtry:[{
+      mode: 'scaleToFill',
+      text: '请假',
+      pic: 'http://i1.bvimg.com/650276/165e12bb3a6347c8.png',
+    }, {
+      mode: 'scaleToFill',
+      text: '报销',
+      pic: 'http://i2.bvimg.com/650276/bee581c260d40094.png',
+      }, {
+        mode: 'scaleToFill',
+        text: '外出',
+        pic:'http://i1.bvimg.com/650276/3f08372be002da58.png',
+    }, {
+      mode: 'scaleToFill',
+      text: '采购',
+      pic: 'http://i1.bvimg.com/650276/7965881414f50dc9.png',
+      }, {
+        mode: 'scaleToFill',
+        text: 's',
+        pic: 'http://i2.bvimg.com/650276/bee581c260d40094.png',
+    }],
+    contents:[{
+      text1:'日',
+      text2:'日报',
+    }, {
+      text1: '周',
+      text2: '周报',
+      }, {
+        text1: '月',
+        text2: '月报',
+      },],
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
